8d5a34817de3d40d035aa4f47c0948e59ab989dd
Pasidu Sachintha <143531800+Pasidu09-SL@users.noreply.github.com>
2024-07-22 16:34:48 +0530

    Updated
    Animations Added
    Meta tag Added
    Changing Theme Button Added
    My Skills Added
    Email send successful alert added
    Mouse Ripple Added

--- js/email.js
+++ js/email.js
@@ -1,3 +1,26 @@
-  (function(){
-    emailjs.init("cT7HiVfepeiACZYll"); // Replace with your EmailJS user ID
-  })();
+
+
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+  event.preventDefault();
+  const name = document.getElementById('name').value
+  const email = document.getElementById('email').value
+  const message = document.getElementById('message').value
+  const subject = document.getElementById('subject').value
+
+  const template = {
+      name: name,
+      email: email,
+      message: message,
+      subject: subject
+  }
+  emailjs.send('service_647f3ya', 'template_15puhbk', template)
+      .then(function restForm() {
+          console.log('Message sent successfully!');
+          document.getElementById("contact-form").reset();
+          alert("Your Messege Sent Successfully !!");
+      }, function (error) {
+          console.log('Failed to send message. Please try again.');
+          console.log('FAILED...', error);
+          alert("Failed to Send Message. Please try again !!");
+      });
+});
--- js/script.js
+++ js/script.js
@@ -1,20 +1,42 @@
-var tablinks = document.getElementsByClassName("tab-links");
-        var tabcontents = document.getElementsByClassName("tab-contents");
-        var sidem = document.getElementById("sidemenu")
+// =================About Me Tabs================
 
-        function opentab(tabname) {
-            for (tablink of tablinks) {
-                tablink.classList.remove("active-link");
-            }
-            for (tabcontent of tabcontents) {
-                tabcontent.classList.remove("active-tab");
-            }
-            event.currentTarget.classList.add("active-link");
-            document.getElementById(tabname).classList.add("active-tab");
-        }
-        function openmenu() {
-            sidem.style.right = "0";
-        }
-        function closemenu() {
-            sidem.style.right = "-200px";
-        }
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// =================Side Menu================
+
+var sidem = document.getElementById("sidemenu")
+function openmenu() {
+    sidem.style.right = "0";
+}
+function closemenu() {
+    sidem.style.right = "-200px";
+}
+
+
+// =================Mose Movement Tracking For Ripple ================
+
+document.addEventListener('mousemove', function (e) {
+    const ripple = document.createElement('div');
+    ripple.className = 'ripple';
+    ripple.style.left = `${e.pageX - 10}px`;
+    ripple.style.top = `${e.pageY - 10}px`;
+
+    document.body.appendChild(ripple);
+
+    // Remove the ripple element after animation ends
+    ripple.addEventListener('animationend', function () {
+        ripple.remove();
+    });
+});
